eddae2779b60dab4458862c6ece0d40bfda15b4b
Owen Miller <owen9825@gmail.com>
2025-11-09 12:34:32 +1100

    Correcting type for climate video

--- climateVideoInjection.js
+++ climateVideoInjection.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var videoSrc = 'https://fusionparty.space/video/climate_stripe_video_trimmed.webm';
       var videoHtml = `
         <video width="100%" autoplay="" loop="" muted="" playsinline="" class="rounded">
-          <source src="${videoSrc}" type="video/mp4">
+          <source src="${videoSrc}" type="video/webm">
           Your browser must not support the video tag.
         </video>
       `;
